ec11387e7030e4b7c34df7219c16bc9a5a0295ed
Ubaidullah-Hasan <hm2964133@gmail.com>
2023-12-29 22:59:38 +0600

    reviews data fetch

--- src/Pages/Home/Testemonial/Testemonial.jsx
+++ src/Pages/Home/Testemonial/Testemonial.jsx
@@ -1,10 +1,27 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import SectionHeader from '../../../Components/SectionHeader';
 
 const Testemonial = () => {
+    const [reviews, setReviews] = useState([]);
+    useEffect(() => {
+        fetch('reviews.json')
+            .then(res => res.json())
+            .then(data => setReviews(data))
+    }, [])
+    console.log(reviews);
+
+
     return (
-        <div>
-            
-        </div>
+        <section>
+            <SectionHeader
+                subHeading={"What Our Clients Say"}
+                title={"TESTIMONIALS"}
+            />
+
+            <div>
+                
+            </div>
+        </section>
     );
 };
 
